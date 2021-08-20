1abbd6a542ad978052a6f8a505aa374c4bd66166
tarundubai <59069453+tarundubai@users.noreply.github.com>
2021-08-20 21:25:53 +0400

    Update kube.ps1

--- kube.ps1
+++ kube.ps1
@@ -1,2 +1,2 @@
 kubectl.exe  get pod -n systest --kubeconfig=C:\Users\TarunB-PC\.kube\config-laptop1 | Out-File -FilePath E:\tarun.txt
-kubectl.exe  run nginx-test-test --image=nginx -n systest --kubeconfig=C:\Users\TarunB-PC\.kube\config-laptop1 | Out-File -FilePath --Append E:\tarun.txt
+kubectl.exe  run nginx-test-test --image=nginx -n systest --kubeconfig=C:\Users\TarunB-PC\.kube\config-laptop1 | Out-File -Append -FilePath E:\tarun.txt
