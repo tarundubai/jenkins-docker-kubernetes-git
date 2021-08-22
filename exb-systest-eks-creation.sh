009defb9f0a483a83d18189ad21638a2092ed65a
tarundubai <59069453+tarundubai@users.noreply.github.com>
2021-08-22 16:26:28 +0400

    Update exb-systest-eks-creation.sh

--- exb-systest-eks-creation.sh
+++ exb-systest-eks-creation.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # This script will create a system test env of TSM server on AWS EKS  compirses TSM server , selenium/standalone-chrome , browsermob-proxy and robot framework
 # Get an AWS IAM user with fixed Access key and Security Access Key and Permission to access on EKS
+
 # Need to add in EKS aws-auth config file
 #aws configure        # This has to be done all workers agent nodes in Teamcity 
+
 # Install helm
 # Install kubectl in all workers agent nodes in Teamcity 
 #curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.20.4/2021-04-12/bin/linux/amd64/kubectl
@@ -10,7 +12,9 @@
 #mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 #echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 #kubectl version --short --client
+
 # Get the kubeconfig file from EKS with AWS IAM user access key and secure access key
+
 # create a namespace 'systest'
 kubectl apply -f systest-create-ns.yaml --kubeconfig=/home/tarun/.kube/config-tsm-dev --dry-run=client > /home/tarun/exb/logs/systest-create-ns.log
 # Get the HELM chart from bitbucket and create tsm server named with tsm-systest
@@ -47,5 +51,5 @@ sleep 10
 kubectl cp  $TEST_DIR/fixtures  systest/$POD_NAME:/home/robot/test  --kubeconfig=/home/tarun/.kube/config-tsm-dev -n systest
 sleep 10
 # Execute the test cases
-kubectl exec -it $POD_NAME -n systest --kubeconfig=/home/tarun/.kube/config-tsm-dev sh
-#wait-for-it.sh -t 60 tsm:8888 -- robot -d output .  >> /home/tarun/exb/logs/robot.log
+kubectl exec -it $POD_NAME -n systest --kubeconfig=/home/tarun/.kube/config-tsm-dev sh > /home/tarun/exb/logs/robot-login.log
+wait-for-it.sh -t 60 tsm:8888 -- robot -d output .  > /home/robot/output/robot.log
